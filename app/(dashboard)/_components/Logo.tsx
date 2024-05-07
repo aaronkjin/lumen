e20a90c81e4c44ae40e2b23772b96065e59a607b
aaronkjin <58490258+hoxykidmilli@users.noreply.github.com>
2024-05-06 21:41:59 -0700

    logo

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,5 @@
+import Image from "next/image";
+
+export const Logo = () => {
+  return <Image height={36} width={36} alt="logo" src="/logo.svg" />;
+};
--- app/(dashboard)/_components/Sidebar.tsx
+++ app/(dashboard)/_components/Sidebar.tsx
@@ -1,3 +1,11 @@
+import { Logo } from "./Logo";
+
 export const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <div className="p-6">
+        <Logo />
+      </div>
+    </div>
+  );
 };
